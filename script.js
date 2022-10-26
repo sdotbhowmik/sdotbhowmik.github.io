2330578ac7e5af2ce9a6796991554fe3dbe8da67
sdotbhowmik <kingshuvo@gmail.com>
2022-10-26 22:44:02 +0600

    Code Modified

--- script.js
+++ script.js
@@ -70,3 +70,39 @@ $(document).ready(function(){
         }
     });
 });
+
+/*== Accordion skills ==*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
+
+/*== Qualification tabs==*/
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target);
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active');
+        })
+        target.classList.add('qualification__active');
+        tabs.forEach(tabss =>{
+            tabss.classList.remove('qualification__active');
+        })
+        tab.classList.add('qualification__active');
+
+    })
+})
